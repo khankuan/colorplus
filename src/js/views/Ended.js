19d5ea98e97bfbe4fa53b1690833653b1a340f1a
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-07-15 22:47:24 +0800

    Implement Facebook Integration

--- src/js/views/Ended.js
+++ src/js/views/Ended.js
@@ -16,10 +16,24 @@ class Ended extends React.Component {
     window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`, '', `menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=${h},width=${w},top=${top},left=${left}`);
   }
 
+  handleFbShare () {
+    let message = `I scored ${this.props.gameStore.score} points at COLOR+, a game where you guess a combination of colors! #colorplus`.toString();
+    // const w = 600;
+    // const h = 300;
+    // const left = (screen.width / 2) - (w / 2);
+    // const top = (screen.height / 2) - (h / 2);
+    FB.ui({
+      method: 'feed',
+      description: message,
+      caption: 'COLOR+ ~ can you score higher than me??',
+      link: 'http://khankuan.github.io/colorplus',
+      picture: 'http://khankuan.github.io/colorplus/picture.png'
+    }, function(response){});
+  }
+
   render (){
     return (
       <div className='ended'>
-
         <p className='game-over'>GAME OVER!</p>
 
         <div>
@@ -28,12 +42,21 @@ class Ended extends React.Component {
 
         <br />
         <br />
-        <br />
 
         <div>
           <a onClick={this.handleShare.bind(this)}
-             target="_blank">
+             target='_blank'>
              Share on Twitter
+          </a>
+        </div>
+
+        <br />
+        <br />
+
+        <div>
+          <a onClick={this.handleFbShare.bind(this)}
+            target='_blank'>
+            Share on Facebook
           </a>
         </div>
 
